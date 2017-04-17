96df53f054f81e6ed64c67d987ef8632797b346e
A.I <ailis@paw.zone>
2017-04-16 18:52:46 -0700

    Stop all videos on a Mastodon dashboard

---
+++ userscript.mastodon.stop.videos.tbrl.js
@@ -0,0 +1,36 @@
+// ==Taberareloo==
+// {
+//   "name"        : "Stop Videos on a Mastodon Dashboard"
+// , "description" : "Stop all videos on a Mastodon dashboard"
+// , "include"     : ["content"]
+// , "match"       : ["*://*/*"]
+// , "version"     : "0.1.0"
+// , "downloadURL" : "https://raw.githubusercontent.com/ailispaw/taberareloo-mastodon/master/userscript.mastodon.stop.videos.tbrl.js"
+// }
+// ==/Taberareloo==
+
+(function() {
+  if (!$X('.//div[@data-react-class="Mastodon"]')[0]) return;
+
+  var timer = null;
+
+  function stopVideos () {
+    if (timer) {
+      clearTimeout(timer);
+      timer = null;
+    }
+
+    var videos = document.getElementsByTagName('video');
+    for (var i = 0 ; i < videos.length ; i++) {
+      if (!videos[i].getAttribute('controls')) {
+        videos[i].pause();
+        videos[i].loop = false;
+        videos[i].setAttribute('controls', 'controls');
+      }
+    }
+
+    timer = setTimeout(stopVideos, 1000);
+  }
+
+  stopVideos();
+})();
